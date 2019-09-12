5d9ffeec57a6117eb4508e9e4987e09e25878da0
Aoyagi-kozue <sbrn.chtsk@icloud.com>
2019-09-12 12:14:02 +0000

    [Finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f0c60e3e4d2bf0ba462b13e87ee729312c1e51023cdb24f7cf74debc8c0e28f528bbfe22fc80d93ffdf945c0ba8cbb69d7052a99899a3c83f157be4e47fc8cab'
